d3f1af1e55e8faaf3478d686434eb135c8089bae
Suryanto <67571269+suryanto23@users.noreply.github.com>
2021-02-16 18:49:39 +0700

    upload file-file utama

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+
+
+class Enemy {
+
+    constructor(name, speed, hp, ap, isDead){
+
+
+
+
+    }
+
+
+};
+
+class Pocong extend Enemy {
+
+
+
+    constructor(name, speed, hp, ap, isDead){
+
+
+
+
+    }
+
+
+};
+
+class Zombie extend Enemy {
+
+
+
+    constructor(name, speed, hp, ap, isDead){
+
+
+
+
+    }
+
+
+};
